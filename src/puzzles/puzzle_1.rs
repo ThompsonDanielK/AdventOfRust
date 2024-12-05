ffbf2767e63e8ba3a0c49056b7ba3452d18b7087
Dan Thompson <ThompsonDanielK@gmail.com>
2024-12-04 21:50:20 -0500

    Update puzzle_1.rs
    puzzle_1 variable name cleanup

--- src/puzzles/puzzle_1.rs
+++ src/puzzles/puzzle_1.rs
@@ -1,37 +1,37 @@
 pub fn solve(text_contents: Vec<String>) -> i32 {
-    let (mut first_list, mut second_list) = split_into_two_lists(text_contents);
+    let (mut first_vec, mut second_vec) = split_into_two_vec(text_contents);
 
-    first_list.sort();
-    second_list.sort();
+    first_vec.sort();
+    second_vec.sort();
 
-    let result = calculate_total_distance(first_list, second_list);
+    let result = calculate_total_distance(first_vec, second_vec);
     result
 }
 
-fn split_into_two_lists(input: Vec<String>) -> (Vec<i32>, Vec<i32>) {
-    let mut first_list = Vec::new();
-    let mut second_list = Vec::new();
+fn split_into_two_vec(input: Vec<String>) -> (Vec<i32>, Vec<i32>) {
+    let mut first_vec = Vec::new();
+    let mut second_vec = Vec::new();
 
     for line in input {
         let mut parts = line.split_whitespace();
         if let Some(first) = parts.next() {
             if let Ok(parsed_first) = first.parse::<i32>() {
-                first_list.push(parsed_first);
+                first_vec.push(parsed_first);
             }
         }
         if let Some(second) = parts.next() {
             if let Ok(parsed_second) = second.parse::<i32>() {
-                second_list.push(parsed_second);
+                second_vec.push(parsed_second);
             }
         }
     }
 
-    (first_list, second_list)
+    (first_vec, second_vec)
 }
 
-fn calculate_total_distance(first_list: Vec<i32>, second_list: Vec<i32>) -> i32 {
+fn calculate_total_distance(first_vec: Vec<i32>, second_vec: Vec<i32>) -> i32 {
     let mut total_distance: i32 = 0;
-    for (first, second) in first_list.iter().zip(second_list.iter()) {
+    for (first, second) in first_vec.iter().zip(second_vec.iter()) {
         let difference = (first - second).abs();
         total_distance += difference;
     }
